9ebfd32d9baba365bece3a6203e1f7a00824699c
esrabaldiz <esracagan85@gmail.com>
2023-02-02 13:28:16 +0100

    Mesaj1

---
+++ src/main/java/day36lambda/Lambda02.java
@@ -0,0 +1,47 @@
+package day36lambda;
+
+public class Lambda02 {
+
+    /*
+
+    Universite u1 = new Universite("hacettepe","fizik",1200,88);
+    Universite u2 = new Universite("bogazici","qa",1000,90);
+    Universite u3 = new Universite("odtu","dev",900,95);
+    Universite u4 = new Universite("marmara","matematik",3000,60);
+    Universite u5 = new Universite("ege","elektrik-elektronik",2000,83);
+
+    List<Universite> unv = new ArrayList<>(Arrays.asList(u1,u2,u3,u4,u5));
+        System.out.println(notOrt75tenByk(unv));
+        System.out.println();
+        System.out.println(mtkVrmYkm(unv));//true
+        System.out.println();
+
+
+
+}//main
+
+    // Task 01 : Butun universitelerin not ortalamasinin 75'ten buyuk oldugunu
+    // gosteren bir program olusturunuz.
+    public static boolean notOrt75tenByk(List<Universite> unv){
+        return  unv.
+                stream().
+                allMatch(t->t.getNotOrtalamasi()>75);
+
+    }
+
+    // Task 02 : Universitelerden herhangi birinde matematik bolumu olup olmadigini kontrol eden
+    //  bir program olusturunuz.
+    public static boolean mtkVrmYkm(List<Universite> unv){
+        return  unv.
+                stream().
+                anyMatch(t->t.getBolum().equalsIgnoreCase("matematik"));
+
+    }
+
+    // Task 03 : Universiteleri ogrenci sayilarina gore buyukten kucuge siralayan
+    //    bir program olusturunuz.
+
+}//class
+
+     */
+}
